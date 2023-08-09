d20fb8c3c8e1ea1f5c5c59c5dff1b581f5c8d4ad
José Guilherme Lins Filho <jose.filho@elecnor.com.br>
2023-08-09 11:56:48 -0400

    correction final

--- Program.cs
+++ Program.cs
@@ -8,52 +8,36 @@ namespace Calculadora
         {
             bool continuarLoop = true;
 
-            while (continuarLoop){
+            while (continuarLoop)
+            {
 
                 Console.Clear();
                 Console.WriteLine("CALCULADORA");
-                Console.WriteLine("DIGITE A OPERAÇÃO QUE DESEJA REALIZA\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Sair\n");            
+                Console.WriteLine("DIGITE A OPERAÇÃO QUE DESEJA REALIZAR\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Sair\n");
                 int escolha = int.Parse(Console.ReadLine());
-                
+
                 Console.Clear();
-                
-                if (escolha == 5){
+
+                if (escolha == 5)
+                {
                     continuarLoop = false;
                     break;
-                }else if (escolha > 5){
-                    Console.WriteLine("\nOpção Invalida\n");
+                }
+                else if (escolha > 5)
+                {
+                    Console.WriteLine("\nOpção Invalida");
                     continuarLoop = false;
                     break;
-                }else{
-                    Console.WriteLine("Digite o primeiro valor: ");
-                    float valor1 = float.Parse(Console.ReadLine());
-                    Console.WriteLine("Digite o segundo valor: ");
-                    float valor2 = float.Parse(Console.ReadLine());
-
-                    Console.Clear();
-                    switch (escolha) {
-                        case 1: 
-                            Soma(valor1, valor2);
-                            break;
-                        case 2:
-                            Subtracao(valor1, valor2);
-                            break;
-                        case 3:
-                            Multiplicacao(valor1, valor2);  
-                            break;
-                        case 4:
-                            Divisao(valor1, valor2);
-                            break;
-                    }
-
-                    Console.WriteLine("\nTecle `ENTER` para continuar...\n");
-                    Console.ReadKey();
+                }
+                else
+                {
+                    Menu(escolha);
                 }
             }
         }
-        
+
         static void Soma(float numero1, float numero2)
-        {   
+        {
             float soma = numero1 + numero2;
             string resultado = $"\nA soma dos valores é {soma}";
             Console.WriteLine(resultado);
@@ -78,6 +62,34 @@ namespace Calculadora
             float divisao = numero1 / numero2;
             string resultado = $"\nA divisão dos valores é {divisao}";
             Console.WriteLine(resultado);
+        }
+
+        static void Menu(int escolha)
+        {
+            Console.WriteLine("Digite o primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            Console.Clear();
+            switch (escolha)
+            {
+                case 1:
+                    Soma(valor1, valor2);
+                    break;
+                case 2:
+                    Subtracao(valor1, valor2);
+                    break;
+                case 3:
+                    Multiplicacao(valor1, valor2);
+                    break;
+                case 4:
+                    Divisao(valor1, valor2);
+                    break;
+            }
+
+            Console.WriteLine("\nTecle `ENTER` para continuar...\n");
+            Console.ReadKey();
         }
     }
 
